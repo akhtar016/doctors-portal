296a6414bb22ae01d6d66fd959dff5cb273f62c5
theDreamerNYM <akhtaruzzamankhan016@gmail.com>
2020-04-12 18:01:24 +0600

    popup box issue is solved

--- src/Components/FirstSection/MiddlePart/Appointment/CardViewAppointment/BookAppointment/BookAppointment.js
+++ src/Components/FirstSection/MiddlePart/Appointment/CardViewAppointment/BookAppointment/BookAppointment.js
@@ -5,10 +5,8 @@ import { useForm } from "react-hook-form";
 import "./BookAppointment.css";
 import PopUp from "../../../../../PopUp/PopUp";
 
-
 const BookAppointment = (props) => {
   const { key } = useParams();
-
 
   const [modalShow, setModalShow] = React.useState(false);
 
@@ -22,18 +20,12 @@ const BookAppointment = (props) => {
 
   const { register, handleSubmit, errors } = useForm();
 
-  const showModal = () => setModalShow(true);
+  const showModal = () => {
+    setModalShow(true);
+  };
 
-  
-  
   const onSubmit = (data) => {
-
-    
     console.log(data);
-
-    showModal();
-
-    
 
     // post data to server
 
@@ -41,149 +33,118 @@ const BookAppointment = (props) => {
       method: "POST",
       body: JSON.stringify(data),
       headers: {
-        "Content-type": "application/json"
+        "Content-type": "application/json",
       },
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log("Added data",data);
-        
-      // // get data from database
+        console.log("Added data", data);
 
-      //  fetch('https://doctor-portals.herokuapp.com/patientInfo')
-      //  .then(res => res.json())
-      //  .then(data=> {
-      //    console.log("Data from database: " ,data[data.length - 1]._id);
-      //  })
+        // get data from database
 
+        fetch("https://doctor-portals.herokuapp.com/patientInfo")
+          .then((res) => res.json())
+          .then((data) => {
+            console.log("Data from database: ", data[data.length - 1]._id);
 
-
+            showModal();
+          });
       });
 
     // clear input field
 
     document.getElementById("form-clear").reset();
-
-     
-      
-
-      document.getElementById('spanText').innerText = "Your appointment request is successful";
-
-
-      //  // get data from database
-
-      //  fetch('https://doctor-portals.herokuapp.com/patientInfo')
-      //  .then(res => res.json())
-      //  .then(data=> {
-      //    console.log("Data from database: " ,data[0]._id);
-      //  })
-      
-
-     
   };
-
- 
 
   return (
     <div className="book-appointment">
-      
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          id="form-clear"
-          className="py-5"
-        >
-          <h2
-            className="text-center text-info"  
-          >
-            {appointment.name}
-          </h2>
-          <div className="form-group">
-            <input
-              name="name"
-              className="form-control"
-              ref={register({ required: true })}
-              placeholder="Name"
-            />
-            {errors.name && <span className="error">Name is required</span>}
-          </div>
-          <div className="form-group">
-            <input
-              name="email"
-              className="form-control"
-              ref={register({ required: true })}
-              placeholder="Email"
-            />
-            {errors.email && <span className="error">Email is required</span>}
-          </div>
-          <div className="form-group">
-            <input
-              name="phone"
-              className="form-control"
-              ref={register({ required: true })}
-              placeholder="Phone"
-            />
-            {errors.phone && <span className="error">Phone is required</span>}
-          </div>
-          <div className="form-group">
-            <input
-              name="age"
-              className="form-control"
-              ref={register({ required: true })}
-              placeholder="Age"
-            />
-            {errors.age && <span className="error">Age is required</span>}
-          </div>
-          <div className="form-group">
-            <input
-              name="address"
-              className="form-control"
-              ref={register({ required: true })}
-              placeholder="Address"
-            />
-            {errors.address && <span className="error">Address is required</span>}
-          </div>
-          
-          
-          <div className="form-group">
+      <form onSubmit={handleSubmit(onSubmit)} id="form-clear" className="py-5">
+        <h2 className="text-center text-info">{appointment.name}</h2>
+        <div className="form-group">
+          <input
+            name="name"
+            className="form-control"
+            ref={register({ required: true })}
+            placeholder="Name"
+          />
+          {errors.name && <span className="error">Name is required</span>}
+        </div>
+        <div className="form-group">
+          <input
+            name="email"
+            className="form-control"
+            ref={register({ required: true })}
+            placeholder="Email"
+          />
+          {errors.email && <span className="error">Email is required</span>}
+        </div>
+        <div className="form-group">
+          <input
+            name="phone"
+            className="form-control"
+            ref={register({ required: true })}
+            placeholder="Phone"
+          />
+          {errors.phone && <span className="error">Phone is required</span>}
+        </div>
+        <div className="form-group">
+          <input
+            name="age"
+            className="form-control"
+            ref={register({ required: true })}
+            placeholder="Age"
+          />
+          {errors.age && <span className="error">Age is required</span>}
+        </div>
+        <div className="form-group">
+          <input
+            name="address"
+            className="form-control"
+            ref={register({ required: true })}
+            placeholder="Address"
+          />
+          {errors.address && <span className="error">Address is required</span>}
+        </div>
+
+        <div className="form-group">
           <label>Selected Service</label>
-            <input
-              name="selectedService"
-              className="form-control"
-              ref={register({ required: true })}
-              disabled
-              defaultValue={appointment.name}
-            />
-            <label>Selected Time</label>
-            <input
-              name="selectedTime"
-              className="form-control"
-              ref={register({ required: true })}
-              disabled
-              defaultValue={appointment.time}
-            />
-            <label>Selected Date</label>
-            <input
-              name="selectedDate"
-              className="form-control"
-              ref={register({ required: true })}
-              disabled
-              defaultValue={date}
-            />
-          </div>
-          <div className="form-group">
-             <button className="btn btn-info btn-block" type="submit">
-              Send
-            </button>
+          <input
+            name="selectedService"
+            className="form-control"
+            ref={register({ required: true })}
+            disabled
+            defaultValue={appointment.name}
+          />
+          <label>Selected Time</label>
+          <input
+            name="selectedTime"
+            className="form-control"
+            ref={register({ required: true })}
+            disabled
+            defaultValue={appointment.time}
+          />
+          <label>Selected Date</label>
+          <input
+            name="selectedDate"
+            className="form-control"
+            ref={register({ required: true })}
+            disabled
+            defaultValue={date}
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-info btn-block" type="submit">
+            Send
+          </button>
+        </div>
+      </form>
 
-            <span id="spanText" style={{color:'green'}}></span>
-          </div>
-        </form>
-
-      <PopUp date={date}
+      <PopUp
+        date={date}
         show={modalShow}
+        appointment={appointment}
         onHide={() => setModalShow(false)}
       />
-    
     </div>
   );
 };
--- src/Components/PopUp/PopUp.js
+++ src/Components/PopUp/PopUp.js
@@ -1,50 +1,37 @@
-import React from 'react';
-import { Modal } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Modal } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const PopUp = (props) => {
 
- // get data from database
+  const date = props.date;
+  const service = props.appointment.name;
+  const time = props.appointment.time;
 
- fetch('https://doctor-portals.herokuapp.com/patientInfo')
- .then(res => res.json())
- .then(data=> {
-   console.log("Data from database: " ,data[data.length - 1]._id);
- })
-
- 
-
-
-
-    return (
-        
-        <Modal
+  console.log(date, service, time);
+  return (
+    <Modal
       {...props}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-        
-      <Modal.Header closeButton>
+      <Modal.Header>
         <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+          Your Appointment Status and Info
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4 style={{color:'red'}}>Your Appointment Request is Successful</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+                <p style={{color:'black', fontSize:'14px'}}><span style={{color:'green'}}>Congratulation</span>,Your appointment for {service} is confirmed on {date}. You are requested to visit doctor between {time}. For more: <a href="https://bit.ly/doctors-portal">https://bit.ly/doctors-portal</a><br/>Thank you.</p>
       </Modal.Body>
       <Modal.Footer>
-        
-        <Link to="/"><Button>Okay</Button></Link>
+        <Link to="/">
+          <Button className="btn btn-info">Okay</Button>
+        </Link>
       </Modal.Footer>
     </Modal>
-    );
+  );
 };
 
 export default PopUp;
